a1a90ecd02d9494c1c27919b444fd95f5b6176a5
Magnus Järnhandske <109724162+PickleTheRick@users.noreply.github.com>
2023-05-24 08:15:58 +0200

    Update RUN_FOR_EXAM.bat

--- RUN_FOR_EXAM.bat
+++ RUN_FOR_EXAM.bat
@@ -3,8 +3,8 @@
 :loop
 cls
 echo [1;34mDear Secops, Congratulations on your Exam![0m
-timeout /t 1 >nul
+timeout /t 2 >nul
 cls
-echo [1;31mNow let's go catch some bad guys[0m
-timeout /t 1 >nul
+echo [1;31mNow let's go catch some bad guys![0m
+timeout /t  >nul
 goto loop
