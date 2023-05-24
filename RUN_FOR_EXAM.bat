80ee6d55c67e41a0fb3cd6fa4865dbe8704a3604
Magnus Järnhandske <109724162+PickleTheRick@users.noreply.github.com>
2023-05-24 07:48:53 +0200

    Add files via upload

---
+++ RUN_FOR_EXAM.bat
@@ -0,0 +1,10 @@
+@echo off
+
+:loop
+cls
+echo [1;34mDear Secops, Congratulations on your Exam![0m
+timeout /t 1 >nul
+cls
+echo [1;31mNow let's go catch some bad guys[0m
+timeout /t 1 >nul
+goto loop
